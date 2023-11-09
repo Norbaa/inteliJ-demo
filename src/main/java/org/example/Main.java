1c163a348c28bccc25cd2913275a105126ada892
Bálint Norbert <norbaa79@gmail.com>
2023-11-09 16:27:31 +0100

    Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello world!");
+        System.out.println("Hello fiúk és lányok");
 
     }
+
 }
